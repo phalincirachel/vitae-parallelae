f0509e29d37ef1b77018ab32e885c6de77722915
Stephana0fd9 <Stephan.Pfalzgraf1@gmx.de>
2026-03-16 14:59:03 +0100

    update

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ async function resolveStartupTarget() {
 
     const intro = save && typeof save === 'object' ? save.intro : null;
     const introCompleted = !!(intro && intro.completed === true && Number(intro.version) === Number(INTRO_VERSION));
-    return introCompleted ? INTRO_ROUTE.gameFile : INTRO_ROUTE.introFile;
+    // BYPASS INTRO AS REQUESTED BY USER
+    return INTRO_ROUTE.gameFile;
+    // return introCompleted ? INTRO_ROUTE.gameFile : INTRO_ROUTE.introFile;
 }
 
 async function createWindow() {
